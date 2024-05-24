fce0154a419ff14cd725704cedaf0c62f679a0d1
Piyush Kumar <rajpiyush220@gmail.com>
2024-05-24 23:20:08 +0530

    Time: 2 ms (55.55%) | Memory: 41.4 MB (41.82%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() == 1) return false;
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            char current = s.charAt(i);
+            if(current == '(' || current == '{' || current == '[') stack.push(current);
+            else{
+                if(stack.isEmpty()) return false;
+                char peek = stack.peek();
+                if(
+                    (current == ')' && peek == '(') || 
+                    (current == '}' && peek == '{') ||
+                    (current == ']' && peek == '[')) stack.pop();
+                else return false;
+            }
+        }
+        if(!stack.isEmpty()) return false;
+        return true;
+    }
+}
