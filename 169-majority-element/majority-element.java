abf4b2c9d02bdb54298ff73b2eb0902fa1fd99fb
Piyush Kumar <rajpiyush220@gmail.com>
2024-05-24 09:29:32 +0530

    Time: 3 ms (61.82%) | Memory: 53 MB (32.80%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int majorityElement(int[] nums) {
+    /*public int majorityElement(int[] nums) {
      HashMap<Integer,Integer> countValMap = new HashMap<>();
      for(int i = 0; i < nums.length; i++){
         int preCount = countValMap.getOrDefault(nums[i], 0);
@@ -8,5 +8,34 @@ class Solution {
         countValMap.put(nums[i], preCount);
      }   
      return -1;
+    }*/
+    public int majorityElement(int[] nums) {
+        int candidate = findCandidate(nums);
+        return verifyCandidate(nums, candidate) ? candidate : -1;
+    }
+
+    private int findCandidate(int[] nums) {
+        int candidate = 0, count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+
+    private boolean verifyCandidate(int[] nums, int candidate) {
+        int count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+        return count > nums.length / 2;
     }
 }
