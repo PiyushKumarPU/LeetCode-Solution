47d12420fae60f720e4023e25a085ef85387a5c9
Piyush Kumar <rajpiyush220@gmail.com>
2024-05-31 18:35:06 +0530

    Time: 5 ms (86.70%) | Memory: 44.2 MB (79.33%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -5,12 +5,13 @@ class Solution {
         int start = 0, end = 0, maxLength = 0;
         while (end < str.length()) {
             if (valueIndexMap.containsKey(str.charAt(end))) {
-                //if(start < currentIndex) start = currentIndex;
-                start = Math.max(start, valueIndexMap.get(str.charAt(end)) + 1);
+                int currentIndex = valueIndexMap.get(str.charAt(end)) + 1;
+                if (start < currentIndex) start = currentIndex;
+                // removed above one and added max so that start wont go back
+                // start = Math.max(start, valueIndexMap.get(str.charAt(end)) + 1);
             }
             valueIndexMap.put(str.charAt(end), end);
             maxLength = Math.max(maxLength, (end - start + 1));
-
             end++;
         }
         return maxLength;
