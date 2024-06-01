efe609c326d3c3e7bd9115031954518b15addbbb
Piyush Kumar <rajpiyush220@gmail.com>
2024-06-01 15:15:32 +0530

    Time: 475 ms (5.01%) | Memory: 47.6 MB (73.58%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,21 +1,33 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> anagramMap = new HashMap<>();
-        for (String str : strs) {
-            char[] sorted = str.toCharArray();
-            Arrays.sort(sorted);
-            String sortedStr = new String(sorted);
-            if (anagramMap.containsKey(sortedStr)) anagramMap.get(sortedStr).add(str);
-            else {
-                List<String> row = new ArrayList<>();
-                row.add(str);
-                anagramMap.put(sortedStr, row);
-            }
-        }
         List<List<String>> result = new ArrayList<>();
-        for(Map.Entry<String,List<String>> entry : anagramMap.entrySet()){
-            result.add(entry.getValue());
+        if (strs == null || strs.length == 0) return result;
+        for (int i = 0; i < strs.length; i++) {
+            if (Objects.equals(strs[i], "-1")) continue;
+            List<String> row = new ArrayList<>();
+            row.add(strs[i]);
+            int start = i + 1;
+            while (start < strs.length) {
+                if (strs[i].length() == strs[start].length() && strs[start] != "-1") {
+                    if (isAnagram(strs[i], strs[start])) {
+                        row.add(strs[start]);
+                        strs[start] = "-1";
+                    }
+                }
+                start++;
+            }
+            result.add(row);
         }
         return result;
+    }
+
+    public boolean isAnagram(String s, String t) {
+        int[] charCounts = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            charCounts[s.charAt(i) - 'a']++;
+            charCounts[t.charAt(i) - 'a']--;
+        }
+        for (int count : charCounts) if (count != 0) return false;
+        return true;
     }
 }
