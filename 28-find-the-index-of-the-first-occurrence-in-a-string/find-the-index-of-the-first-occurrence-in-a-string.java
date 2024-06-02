c8db421b06746855c1012b0ebd469f414fc52edf
Piyush Kumar <rajpiyush220@gmail.com>
2024-06-02 22:46:17 +0530

    Time: 1 ms (30.97%) | Memory: 41 MB (89.05%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,12 +1,43 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(haystack == null || haystack.isEmpty()) return -1;
-        //if(haystack.length() == needle.length() && haystack.equals(needle) ) return 0;
-        int start = 0, nLen = needle.length();
-        while (start <= haystack.length() - nLen) {
-            if (haystack.substring(start, start + needle.length()).equals(needle)) return start;
-            start++;
+        int j = 0;
+        int[] lps = computeLPSArray(needle);
+        for (int i = 0; i < haystack.length(); ) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                if (j == 0) {
+                    i++;
+                } else {
+                    j = lps[j - 1];
+                }
+            }
+            if (j == needle.length()) {
+                return i - j;
+            }
         }
         return -1;
+    }
+
+    private int[] computeLPSArray(String pattern) {
+        int length = pattern.length();
+        int[] lps = new int[length];
+        int lpsLength = 0;
+        int i = 1;
+
+        while (i < length) {
+            if (pattern.charAt(i) == pattern.charAt(lpsLength)) {
+                lpsLength++;
+                lps[i] = lpsLength;
+                i++;
+            } else if (lpsLength != 0) {
+                lpsLength = lps[lpsLength - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+        return lps;
     }    
 }
