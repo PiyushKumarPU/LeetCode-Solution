f96a7918866fa9c9736a2a42e8d45b41fbbda4be
Piyush Kumar <rajpiyush220@gmail.com>
2024-06-07 21:21:45 +0530

    Time: 5 ms (96.68%) | Memory: 42.4 MB (45.11%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,31 +1,13 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-
-        Map<Character, Character> sToT = new HashMap<>(s.length());
-        Map<Character, Character> tToS = new HashMap<>(s.length());
-
-        for (int i = 0; i < s.length(); i++) {
-            char charS = s.charAt(i);
-            char charT = t.charAt(i);
-
-            if (sToT.containsKey(charS)) {
-                if (sToT.get(charS) != charT) {
-                    return false;
-                }
-            } else {
-                sToT.put(charS, charT);
-            }
-
-            if (tToS.containsKey(charT)) {
-                if (tToS.get(charT) != charS) {
-                    return false;
-                }
-            } else {
-                tToS.put(charT, charS);
-            }
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+        for (int i = 0; i < s.length(); ++i) {
+            char char1 = s.charAt(i);
+            char char2 = t.charAt(i);
+            if (m1[char1] != m2[char2]) return false;
+            m1[char1] = i + 1;
+            m2[char2] = i + 1;
         }
         return true;
     }
