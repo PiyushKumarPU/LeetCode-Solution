bbefaae91dea135c87746aec083d2a3f36b2aa26
Piyush Kumar <rajpiyush220@gmail.com>
2024-06-08 22:39:10 +0530

    Time: 43 ms (66.05%) | Memory: 90.7 MB (78.04%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.java
@@ -1,23 +1,32 @@
 class Solution {
     public int[] queryResults(int limit, int[][] queries) {
-        HashMap<Integer, Integer> ballColorMap = new HashMap<>();
-        HashMap<Integer, Integer> colorCountMap = new HashMap<>();
         int[] result = new int[queries.length];
+        Map<Integer, Integer> ballMap = new HashMap<>((int) (queries.length / 0.75 + 1));
+        Map<Integer, Integer> colorMap = new HashMap<>((int) (queries.length / 0.75 + 1));
 
         for (int i = 0; i < queries.length; i++) {
             int ball = queries[i][0];
             int color = queries[i][1];
 
-            if (ballColorMap.containsKey(ball)) {
-                int existingColor = ballColorMap.get(ball);
-                colorCountMap.put(existingColor, colorCountMap.get(existingColor) - 1);
-                if (colorCountMap.get(existingColor) == 0) {
-                    colorCountMap.remove(existingColor);
+            if (ballMap.containsKey(ball)) {
+                int oldColor = ballMap.get(ball);
+
+                if (color != oldColor) {
+                    int oldColorCount = colorMap.get(oldColor);
+                    ballMap.put(ball, color);
+                    colorMap.put(color, colorMap.getOrDefault(color, 0) + 1);
+
+                    if (oldColorCount == 1) {
+                        colorMap.remove(oldColor);
+                    } else {
+                        colorMap.put(oldColor, oldColorCount - 1);
+                    }
                 }
+            } else {
+                ballMap.put(ball, color);
+                colorMap.put(color, colorMap.getOrDefault(color, 0) + 1);
             }
-            ballColorMap.put(ball, color);
-            colorCountMap.put(color, colorCountMap.getOrDefault(color, 0) + 1);
-            result[i] = colorCountMap.size();
+            result[i] = colorMap.size();
         }
         return result;
     }
