0678aeb633fb693effca4db64385e555d94d6347
Piyush Kumar <rajpiyush220@gmail.com>
2024-06-13 12:27:42 +0530

    Time: 1 ms (94.78%) | Memory: 45.9 MB (11.56%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int pivotIndex(int[] A) {
-       if (A == null || A.length == 0) return 0;
-        else if (A.length == 1) return 0;
+    public int pivotIndex(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        else if (nums.length == 1) return 0;
         // calculate prefix sum array
-        int[] pfSum = new int[A.length];
-        pfSum[0] = A[0];
-        for (int i = 1; i < A.length; i++) {
-            pfSum[i] = A[i] + pfSum[i - 1];
+        int[] pfSum = new int[nums.length];
+        pfSum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            pfSum[i] = nums[i] + pfSum[i - 1];
         }
-        for (int i = 0; i < A.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             int leftSum = i > 0 ? pfSum[i - 1] : 0;
-            int rightSum = i == A.length - 1 ? 0 : pfSum[A.length - 1] - pfSum[i];
+            int rightSum = i == nums.length - 1 ? 0 : pfSum[nums.length - 1] - pfSum[i];
             if (leftSum == rightSum) return i;
         }
-        return -1; 
+        return -1;
     }
 }
