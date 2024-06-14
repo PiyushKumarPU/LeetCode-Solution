ac576ce7aca4b5a91956c2557136449f191b79dc
Piyush Kumar <rajpiyush220@gmail.com>
2024-06-14 22:25:26 +0530

    Time: 1 ms (100.00%) | Memory: 56.9 MB (77.91%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,16 +1,20 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        if(nums.length == 1) return nums[0] == 1 ? 2 : 1;
-        int length = nums.length;
-        int[] result = new int[length];
-        for (int current : nums) {
-            if (current <= 0 || current > length) continue;
-            result[current - 1] = current;
+    public int firstMissingPositive(int[] A) {
+        if(A.length == 1) return A[0] == 1 ? 2 : 1;
+        // check for all the element greater than 0 and less than and equal to size and
+        // keep it at appropriate index like 1 should be at index 0
+        // A[i] should be at index A[i] - 1;
+        for (int i = 0; i < A.length; i++) {
+            while (A[i] >= 1 && A[i] <= A.length && A[i] != A[A[i] - 1]) {
+                int temp = A[i];
+                A[i] = A[A[i] - 1];
+                A[temp - 1] = temp;
+            }
         }
-        for (int i = 0; i < length; i++) {
-            int present = result[i];
-            if (present != (i + 1)) return i + 1;
+        // check first index where it does not have value i + 1
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != i + 1) return i + 1;
         }
-        return length + 1;
+        return A.length + 1;
     }
 }
