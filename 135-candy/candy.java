9ae5040b3fbceabaeee776fa3ea28658ae9f6d1e
Piyush Kumar <rajpiyush220@gmail.com>
2024-06-15 00:19:22 +0530

    Time: 3 ms (82.71%) | Memory: 45.4 MB (61.15%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,19 +1,25 @@
 class Solution {
     public int candy(int[] ratings) {
-        int length = ratings.length;
-        int[] result = new int[length];
+        // idea first assign one candy to each
+        // then start looking right, if current has more rating than right one then it should have more candy than right
+        // then start looking left, if current has more rating then it should get at least one more candy than left
+        int[] result = new int[ratings.length];
         Arrays.fill(result, 1);
-        // check from left to right for higher rank
-        for (int i = 1; i < length; i++) {
-            if (ratings[i] > ratings[i - 1]) result[i] = result[i - 1] + 1;
+        // Check from left to right
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                result[i] = result[i - 1] + 1;
+            }
         }
-        // check from right to left if left is greater than right
-        for (int i = length - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1])
-                result[i] = (Math.max(result[i], result[i + 1] + 1));
+
+        // Check from right to left
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                result[i] = Math.max(result[i], result[i + 1] + 1);
+            }
         }
-        int total = 0;
-        for (int i : result) total += i;
-        return total;
+        int candyCount = 0;
+        for (int count : result) candyCount += count;
+        return candyCount;
     }
 }
