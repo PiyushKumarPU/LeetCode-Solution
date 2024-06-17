16ee32e87d85a0c363391a425f889075fdffbebb
Piyush Kumar <rajpiyush220@gmail.com>
2024-06-17 13:45:45 +0530

    Time: 4 ms (5.78%) | Memory: 41.6 MB (22.54%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,43 +1,27 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        int j = 0;
-        int[] lps = computeLPSArray(needle);
-        for (int i = 0; i < haystack.length(); ) {
-            if (haystack.charAt(i) == needle.charAt(j)) {
-                i++;
-                j++;
-            } else {
-                if (j == 0) {
-                    i++;
-                } else {
-                    j = lps[j - 1];
-                }
-            }
-            if (j == needle.length()) {
-                return i - j;
-            }
+        int[] lps = computeLPSArray(needle + "#" + haystack);
+        for (int i = 0; i < lps.length; i++) {
+            if (lps[i] == needle.length()) return i - needle.length() * 2;
         }
+        // If no match is found, return -1
         return -1;
     }
 
     private int[] computeLPSArray(String pattern) {
-        int length = pattern.length();
-        int[] lps = new int[length];
-        int lpsLength = 0;
-        int i = 1;
-
-        while (i < length) {
-            if (pattern.charAt(i) == pattern.charAt(lpsLength)) {
-                lpsLength++;
-                lps[i] = lpsLength;
-                i++;
-            } else if (lpsLength != 0) {
-                lpsLength = lps[lpsLength - 1];
-            } else {
-                lps[i] = 0;
-                i++;
+        int[] lps = new int[pattern.length()];
+        lps[0] = 0;
+        for (int i = 1; i < pattern.length(); i++) {
+            int x = lps[i - 1];
+            while (pattern.charAt(i) != pattern.charAt(x)) {
+                if (x == 0) {
+                    x = -1;
+                    break;
+                }
+                x = lps[x - 1];
             }
+            lps[i] = x + 1;
         }
         return lps;
-    }    
+    }
 }
