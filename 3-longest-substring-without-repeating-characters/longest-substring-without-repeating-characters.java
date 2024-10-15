e1c5407f3ad59d6d796d1f2d072f76208e90fa0f
PiyushKumarPU <85717008+PiyushKumarPU@users.noreply.github.com>
2024-10-15 13:06:25 +0530

    Time: 1 ms (100.00%) | Memory: 43.4 MB (89.11%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,17 +3,13 @@ class Solution {
         if(s == null || s.isEmpty()) return 0;
          int maxLength = 1, start = 0;
         int[] charIndex = new int[256];
-        // Iterate through the string
         for (int end = 0; end < s.length(); end++) {
             char c = s.charAt(end);
 
-            // Update start position if the character was seen before
             start = Math.max(start, charIndex[c]);
 
-            // Update the character's last seen index (+1 to avoid overlap)
             charIndex[c] = end + 1;
 
-            // Calculate the maximum length so far
             maxLength = Math.max(maxLength, end - start + 1);
         }
         return maxLength;
