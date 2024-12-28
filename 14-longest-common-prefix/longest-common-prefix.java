4cba28d7e952237e187072ea65efbe87c859d133
PiyushKumarPU <85717008+PiyushKumarPU@users.noreply.github.com>
2024-12-28 10:36:57 +0530

    Time: 1 ms (64.19%) | Memory: 41.7 MB (35.84%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,44 +1,25 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
-        TrieBucket root = new TrieBucket();
-
-        // Find the minimum length of the strings in the array
-        int minLength = Integer.MAX_VALUE;
-        for (String str : strs) {
-            if (str.length() < minLength) {
-                minLength = str.length();
+       if (strs == null) return null;
+        else if (strs.length == 1 && strs[0].isEmpty()) return "";
+        StringBuilder minCommon = new StringBuilder("");
+        // find min length String
+        String minLengthStr = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            if (strs[i].length() < minLengthStr.length()) {
+                minLengthStr = strs[i];
             }
         }
-        if(minLength == 0) return "";
-        // Build the Trie up to the length of the shortest string
-        for (String str : strs) {
-            TrieBucket temp = root;
-            for (int i = 0; i < minLength; i++) {
-                char current = str.charAt(i);
-                if (!temp.cache.containsKey(current)) {
-                    temp.cache.put(current, new TrieBucket());
+        // iterate through each character and check if it there at the same index for all the strings
+        for (int i = 0; i < minLengthStr.length(); i++) {
+            char c = minLengthStr.charAt(i);
+            for (String str : strs) {
+                if (str.charAt(i) != c) {
+                    return minCommon.toString();
                 }
-                temp = temp.cache.get(current);
             }
+            minCommon.append(c);
         }
-
-        // Find the longest common prefix
-        StringBuilder builder = new StringBuilder();
-        TrieBucket temp = root;
-        while (temp.cache.size() == 1) {
-            Map.Entry<Character, TrieBucket> entry = temp.cache.entrySet().iterator().next();
-            builder.append(entry.getKey());
-            temp = entry.getValue();
-        }
-        return builder.toString();
-    }
-}
-
-class TrieBucket {
-    Map<Character, TrieBucket> cache;
-
-    public TrieBucket() {
-        cache = new HashMap<>();
+        return minCommon.toString(); 
     }
 }
