d4e2b27bf36191d69181e995426e02687fe1120b
PiyushKumarPU <85717008+PiyushKumarPU@users.noreply.github.com>
2024-12-28 11:07:05 +0530

    Time: 2 ms (65.14%) | Memory: 41 MB (96.13%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,15 +1,19 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        if (s == null || s.isEmpty()) return true;
+        if (s == null || s.isEmpty())
+            return true;
 
-        // start finding from char first and return once char is not present
-        int start = 0, i = 0;
-        for (i = 0; i < s.length(); i++) {
-            while (start < t.length() && s.charAt(i) != t.charAt(start)) start++;
-            if (start == t.length()) return false;
-            start++;
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j))
+                i++;
+            j++;
+
+            if (i == s.length()) {
+                return true;
+            }
         }
-        // check if all character matched
-        return i == s.length();
+
+        return false;
     }
 }
