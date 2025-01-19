d3364370f1a6a3a9850d3d250fff3ef2f22b0a0d
PiyushKumarPU <85717008+PiyushKumarPU@users.noreply.github.com>
2025-01-19 14:41:13 +0530

    Time: 5 ms (65.98%) | Memory: 45 MB (64.62%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,33 +1,31 @@
 class Solution {
     public String convert(String s, int numRows) {
         StringBuilder result = new StringBuilder();
-        List<List<Character>> rows = new ArrayList<>(numRows);
+        List<StringBuilder> rows = new ArrayList<>(numRows);
         for (int i = 0; i < numRows; i++) {
-            rows.add(new ArrayList<>());
+            rows.add(new StringBuilder());
         }
 
         int start = 0, i = 0;
         while (i < s.length()) {
-            start = Math.max(start, 0);
             if (start == 0) {
                 while (start < numRows && i < s.length()) {
-                    rows.get(start).add(s.charAt(i++));
+                    rows.get(start).append(s.charAt(i++));
                     start++;
                 }
             }
             if (start == numRows) {
                 start -= 2;
+                start = Math.max(start, 0);
                 while (start > 0 && i < s.length()) {
-                    rows.get(start).add(s.charAt(i++));
+                    rows.get(start).append(s.charAt(i++));
                     start--;
                 }
             }
         }
 
-        for (List<Character> row : rows) {
-            for (Character character : row) {
-                result.append(character);
-            }
+        for (StringBuilder row : rows) {
+            result.append(row);
         }
         return result.toString();
     }
