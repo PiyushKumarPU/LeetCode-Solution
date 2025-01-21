dbee25c677835b53b3d61d59ad28e06e3d997031
PiyushKumarPU <85717008+PiyushKumarPU@users.noreply.github.com>
2025-01-21 17:49:58 +0530

    Time: 2 ms (88.90%) | Memory: 55.4 MB (73.88%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,21 +1,19 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-       int len = nums.length;
-        int[] leftMultipliers = new int[len];
-        int[] rightMultipliers = new int[len];
-
-        leftMultipliers[0] = 1;
-        rightMultipliers[len - 1] = 1;
-        for (int i = 1; i < len; i++) {
-            leftMultipliers[i] = leftMultipliers[i - 1] * nums[i - 1];
+       if (nums == null) return null;
+        int size = nums.length;
+        if (size == 1) return new int[]{0};
+        int[] result = new int[size];
+        int temp = 1;
+        for (int i = 0; i < size; i++) {
+            result[i] = temp;
+            temp *= nums[i];
         }
-        for (int i = len - 2; i >= 0; i--) {
-            rightMultipliers[i] = rightMultipliers[i + 1] * nums[i + 1];
+        temp = 1;
+        for (int i = size - 1; i >= 0; i--) {
+            result[i] *= temp;
+            temp *= nums[i];
         }
-
-        for (int i = 0; i < len; i++) {
-            nums[i] = leftMultipliers[i] * rightMultipliers[i];
-        }
-        return nums; 
+        return result;
     }
 }
