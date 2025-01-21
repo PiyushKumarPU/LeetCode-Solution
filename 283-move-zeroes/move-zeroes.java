14a24a3d2d1bebc383b6da3723316da23fdf1e64
PiyushKumarPU <85717008+PiyushKumarPU@users.noreply.github.com>
2025-01-21 14:56:41 +0530

    Time: 2 ms (85.09%) | Memory: 46.7 MB (5.41%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,7 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int left = 0,right = 0;
-        
+        int left = 0, right = 0;
         while (left < nums.length) {
             if (nums[left] != 0) {
                 int temp = nums[left];
