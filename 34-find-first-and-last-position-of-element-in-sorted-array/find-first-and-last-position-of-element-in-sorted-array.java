669d846fd6c5d6814927f4af728db54d2377ba74
PiyushKumarPU <85717008+PiyushKumarPU@users.noreply.github.com>
2025-03-24 19:02:39 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (29.56%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,34 +1,33 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-       int[] result = new int[]{-1, -1};
-        if (nums == null) return result;
-        int start = 0, end = nums.length - 1, startIndex = -1;
-        // find first index
+        int[] result = new int[]{-1, -1};
+        if (nums == null || nums.length == 0) return result;
+        // find first occurance
+        int start = 0, end = nums.length - 1;
+        int first = -1, second = -1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                startIndex = mid;
+            if (nums[mid] >= target) {
+                if (nums[mid] == target) first = mid;
                 end = mid - 1;
-            } else if (nums[mid] > target)
-                end = mid - 1;
-            else start = mid + 1;
+            } else start = mid + 1;
         }
-        if (startIndex == -1) return result;
-        result[0] = startIndex;
-        start = startIndex;
+
+        // first == -1 it means element not found
+        if (first == -1)
+            return result;
+        // find second occurance
+        start = first;
         end = nums.length - 1;
-        int endIndex = -1;
-        // find last index
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                endIndex = mid;
+            if (nums[mid] <= target) {
+                if (nums[mid] == target) second = mid;
                 start = mid + 1;
-            } else if (nums[mid] > target)
-                end = mid - 1;
-            else start = mid + 1;
+            } else end = mid - 1;
         }
-        result[1] = endIndex;
+        result[0] = first;
+        result[1] = second;
         return result;
     }
 }
