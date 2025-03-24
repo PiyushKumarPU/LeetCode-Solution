4f7ab57a8afb0d5b857ba7d747c42fd5fb08416a
PiyushKumarPU <85717008+PiyushKumarPU@users.noreply.github.com>
2025-03-24 18:41:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (96.49%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,32 +1,16 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        if (nums == null || nums.length < 1) return 0;
+        if (nums == null || nums.length == 0) return 0;
         int start = 0, end = nums.length - 1;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) end = mid - 1;
-            else start = mid + 1;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
         }
-
-        // control reaches here means element is not available in array
-        if (target < nums[0]) return 0;
-        else if (target > nums[nums.length - 1]) return nums.length;
-        else if (nums.length == 2) return 1;
-        else {
-            // find appropriate place to insert it
-            start = 0;
-            end = nums.length - 1;
-            int currentAns = 0;
-            while (start <= end) {
-                int mid = start + (end - start) / 2;
-                if (nums[mid] > target && nums[mid - 1] < target) currentAns = mid;
-                if (nums[mid] > target)
-                    end = mid - 1;
-                else
-                    start = mid + 1;
-            }
-            return currentAns;
-        }
+        return start;
     }
 }
