ac434c18a7a53d5f3911e2d96ab5e60d85cd6839
PiyushKumarPU <85717008+PiyushKumarPU@users.noreply.github.com>
2025-03-24 19:15:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (55.52%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,17 +1,16 @@
 class Solution {
     public int findPeakElement(int[] nums) {
        if (nums == null || nums.length == 0) return -1;
-        if (nums.length == 1) return 0;
         int start = 0, end = nums.length - 1;
-        while (start <= end) {
+        while (start < end) {
             int mid = start + (end - start) / 2;
-            // check if mid is greater than both neighbours
-            if ((mid - 1 < 0 || nums[mid] > nums[mid - 1]) &&
-                    (mid + 1 >= nums.length || nums[mid] > nums[mid + 1])) {
-                return mid;
-            } else if (mid - 1 < 0 || nums[mid] > nums[mid - 1]) start = mid + 1;
-            else end = mid - 1;
+            // Compare mid with its next neighbor
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
         }
-        return -1; 
+        return start;
     }
 }
