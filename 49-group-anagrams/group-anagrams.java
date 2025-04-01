c18fe57acfd5d8ed043e1f3d107c2dbae63ac9ef
PiyushKumarPU <85717008+PiyushKumarPU@users.noreply.github.com>
2025-04-01 11:28:06 +0530

    Time: 845 ms (5.05%) | Memory: 47.8 MB (69.49%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,33 +1,48 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> result = new ArrayList<>();
-        if (strs == null || strs.length == 0) return result;
-        for (int i = 0; i < strs.length; i++) {
-            if (Objects.equals(strs[i], "-1")) continue;
+        if (strs == null || strs.length == 0) return null;
+        if (strs.length == 1) {
             List<String> row = new ArrayList<>();
-            row.add(strs[i]);
-            int start = i + 1;
-            while (start < strs.length) {
-                if (strs[i].length() == strs[start].length() && strs[start] != "-1") {
-                    if (isAnagram(strs[i], strs[start])) {
-                        row.add(strs[start]);
-                        strs[start] = "-1";
-                    }
+            row.add(strs[0]);
+            result.add(row);
+            return result;
+        }
+
+        for (int i = 0; i < strs.length; i++) {
+            String source = strs[i];
+            strs[i] = "  ";
+            if (Objects.equals(source, "  ")) continue;
+            List<String> row = new ArrayList<>();
+            row.add(source);
+            for (int j = i + 1; j < strs.length; j++) {
+                String target = strs[j];
+                if (Objects.equals(target, "  ")) continue;
+                if (isAnagram(source, target)) {
+                    strs[j] = "  ";
+                    row.add(target);
                 }
-                start++;
             }
             result.add(row);
         }
         return result;
     }
 
-    public boolean isAnagram(String s, String t) {
-        int[] charCounts = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            charCounts[s.charAt(i) - 'a']++;
-            charCounts[t.charAt(i) - 'a']--;
+
+    private boolean isAnagram(String str1, String str2) {
+        if (str1 == null || str2 == null) return false;
+        if (str1.isEmpty() && str2.isEmpty()) return true;
+        if (str1.length() != str2.length()) return false;
+
+        int[] freq = new int[26];
+        for (int i = 0; i < str1.length(); i++) {
+            freq[str1.charAt(i) - 'a']++;
+            freq[str2.charAt(i) - 'a']--;
         }
-        for (int count : charCounts) if (count != 0) return false;
+
+        for (int count : freq) {
+            if (count != 0) return false;
+        }
         return true;
     }
 }
